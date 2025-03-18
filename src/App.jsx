80dc554f9f76fae53f84796349de174c67803605
Josh Endemann <jahend44@gmail.com>
2025-03-18 11:35:05 -0500

    Add CitySearch to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import EventList from "./components/EventList";
+import CitySearch from "./components/CitySearch";
 
 const App = () => {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
